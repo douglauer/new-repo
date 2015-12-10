0b0f18dd9a207f650357389e93611e5e70564133
taky <taky@taky.com>
2015-12-10 06:01:04 -0500

    initial

---
+++ README.md
@@ -0,0 +1,10 @@
+# new-repo
+
+```
+usage: ./new_repo.rb [options]
+    -n, --name         repository name
+    -d, --description  repository description
+    --private          make repository private
+    --help             display usage
+```
+
---
+++ new_repo.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby
+require 'slop'
+require 'json'
+
+opts = Slop.parse do |o|
+  o.string '-n', '--name', 'repository name'
+  o.string '-d', '--description', 'repository description'
+  o.bool '--private', 'make repository private'
+  o.bool '--help', 'display usage'
+end
+
+if !opts[:name] or opts[:help]
+  puts opts
+  exit
+end
+
+if !File.exists?(Dir.home + '/.gitclirc')
+  puts "~/.gitclirc not found"
+  exit
+end
+
+user,pass = File.open(Dir.home + '/.gitclirc','r').read.strip.split ':'
+
+packet = {
+  :name => opts[:name],
+}
+
+if opts[:description]
+  packet[:description] = opts[:description]
+end
+
+if opts[:private]
+  packet[:private] = true
+end
+
+system 'git init'
+
+system """
+  git remote add origin https://github.com/douglauer/#{packet[:name]}.git
+"""
+
+system """
+  curl -u '#{user}:#{pass}' https://api.github.com/user/repos -d '#{packet.to_json}'
+"""
+
